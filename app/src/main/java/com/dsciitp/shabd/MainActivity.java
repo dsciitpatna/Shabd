9fd118ec179be50aa5fbf38f18ad785b3d94a9fe
Amam Jee <amangrobo@gmail.com>
2019-03-11 03:17:44 +0530

    Added home and category fragment

--- app/src/main/java/com/dsciitp/shabd/MainActivity.java
+++ app/src/main/java/com/dsciitp/shabd/MainActivity.java
@@ -1,13 +1,16 @@
 package com.dsciitp.shabd;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HomeFragment.OnCategorySelectedListener {
 
     private TextView mTextMessage;
 
@@ -39,6 +42,59 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        setBaseFragment(savedInstanceState);
     }
 
+    private void setBaseFragment(Bundle savedInstanceState){
+
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            HomeFragment firstFragment = new HomeFragment();
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            firstFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, firstFragment).commit();
+        }
+
+    }
+
+
+    @Override
+    public void onAttachFragment(Fragment fragment) {
+        if (fragment instanceof HomeFragment){
+            HomeFragment homeFragment = (HomeFragment) fragment;
+            homeFragment.setOnCategorySelectedListener(this);
+        }
+    }
+
+
+    public void onCategorySelected(int position){
+        CategoryFragment categoryFragment = new CategoryFragment();
+
+        Bundle args = new Bundle();
+        categoryFragment.setArguments(args);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.fragment_container, categoryFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+    }
 }
