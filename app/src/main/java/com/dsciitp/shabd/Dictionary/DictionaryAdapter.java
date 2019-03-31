45c5546fa326a639faba7b3dbf12edfac4f4cddf
chanmol1999 <chanmol1999@gmail.com>
2019-03-31 20:30:57 +0530

    Implement basic animation in dictionary cards

--- app/src/main/java/com/dsciitp/shabd/Dictionary/DictionaryAdapter.java
+++ app/src/main/java/com/dsciitp/shabd/Dictionary/DictionaryAdapter.java
@@ -1,8 +1,11 @@
 package com.dsciitp.shabd.Dictionary;
-
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Point;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +17,16 @@ import java.util.ArrayList;
 
 public class DictionaryAdapter extends RecyclerView.Adapter<DictionaryAdapter.MyHolder> {
 
-
+    private Context context;
     final private OnCategorySelectedListener callback;
     private ArrayList<String> mylist;
 
-    DictionaryAdapter(ArrayList<String> mylist, Context context,DictionaryAdapter.OnCategorySelectedListener listener) {
+    DictionaryAdapter(ArrayList<String> mylist, Context context, DictionaryAdapter.OnCategorySelectedListener listener) {
         this.mylist = mylist;
         callback = listener;
+        this.context = context;
     }
+
     public interface OnCategorySelectedListener {
         void onTopicSelected(String title);
     }
@@ -35,15 +40,32 @@ public class DictionaryAdapter extends RecyclerView.Adapter<DictionaryAdapter.My
 
     @Override
     public void onBindViewHolder(@NonNull final MyHolder holder, final int position) {
-
         holder.topicTitle.setText(mylist.get(position));
-        holder.topicTitle.setOnClickListener(new View.OnClickListener() {
+        Display display = ((Activity) context).getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        final float screenx = size.x;
+        final float screeny = size.y;
+        holder.alphabet.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                callback.onTopicSelected(mylist.get(holder.getAdapterPosition()));
+            public void onClick(final View v) {
+                final float originalx = holder.alphabet.getX();
+                final float originaly = holder.alphabet.getY();
+                v.animate().x(screenx / 3).y(screeny / 3).translationZBy(25f).scaleX(2.2f).scaleY(2.2f).setDuration(500).withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        v.setClickable(false);
+                        callback.onTopicSelected(mylist.get(holder.getAdapterPosition()));
+                        v.animate().x(originalx).y(originaly).translationZBy(-25f).scaleX(1f).scaleY(1f).setDuration(1000).withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                v.setClickable(true);
+                            }
+                        });
+                    }
+                });
             }
         });
-
     }
 
     @Override
@@ -54,11 +76,12 @@ public class DictionaryAdapter extends RecyclerView.Adapter<DictionaryAdapter.My
     class MyHolder extends RecyclerView.ViewHolder {
         TextView topicTitle;
         ImageView topicBackground;
-
+        CardView alphabet;
         MyHolder(View itemView) {
             super(itemView);
             topicTitle = itemView.findViewById(R.id.topic_title_name);
             topicBackground = itemView.findViewById(R.id.topic_title_background);
+            alphabet = itemView.findViewById(R.id.card_view);
         }
     }
 }
