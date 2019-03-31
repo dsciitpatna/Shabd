92f5b460b23a17a97bd7de83d995b84dfa2820f0
Aman Jee <amangrobo@gmail.com>
2019-03-31 20:20:28 +0530

    Added animation in Basic Fragment

--- app/src/main/java/com/dsciitp/shabd/MainActivity.java
+++ app/src/main/java/com/dsciitp/shabd/MainActivity.java
@@ -2,6 +2,7 @@ package com.dsciitp.shabd;
 
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Point;
 import android.net.Uri;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
@@ -12,6 +13,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Display;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
@@ -25,6 +27,7 @@ import com.dsciitp.shabd.Category.CategoryFragment;
 import com.dsciitp.shabd.Dictionary.DictionaryActivity;
 import com.dsciitp.shabd.Home.HomeFragment;
 import com.dsciitp.shabd.Home.HomeRecyclerAdapter;
+import com.dsciitp.shabd.Home.TopicModel;
 import com.dsciitp.shabd.Learn.LearnActivity;
 import com.dsciitp.shabd.QuickActions.QuickActionFragment;
 import com.dsciitp.shabd.Setting.SettingFragment;
@@ -41,6 +44,7 @@ public class MainActivity extends AppCompatActivity implements HomeRecyclerAdapt
     ImageView del;
     RelativeLayout topbar;
     Resources res;
+
     private static final String TTS_SPEAK_ID = "SPEAK";
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -50,12 +54,12 @@ public class MainActivity extends AppCompatActivity implements HomeRecyclerAdapt
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    topbar=findViewById(R.id.bar);
+                    topbar = findViewById(R.id.bar);
                     topbar.setVisibility(View.VISIBLE);
                     updateFragment(new HomeFragment(), 0);
                     return true;
                 case R.id.navigation_quick:
-                    topbar=findViewById(R.id.bar);
+                    topbar = findViewById(R.id.bar);
                     topbar.setVisibility(View.VISIBLE);
                     updateFragment(new QuickActionFragment(), 1);
                     return true;
@@ -63,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements HomeRecyclerAdapt
                     startActivity(new Intent(MainActivity.this, DictionaryActivity.class));
                     return true;
                 case R.id.navigation_settings:
-                    topbar=findViewById(R.id.bar);
+                    topbar = findViewById(R.id.bar);
                     topbar.setVisibility(View.GONE);
                     updateFragment(new SettingFragment(), 1);
                     return true;
@@ -90,32 +94,22 @@ public class MainActivity extends AppCompatActivity implements HomeRecyclerAdapt
 
     }
 
-    private void setBaseFragment(Bundle savedInstanceState){
+    private void setBaseFragment(Bundle savedInstanceState) {
 
         if (findViewById(R.id.fragment_container) != null) {
 
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
             if (savedInstanceState != null) {
                 return;
             }
-
-            // Create a new Fragment to be placed in the activity layout
             HomeFragment firstFragment = new HomeFragment();
-
-            // In case this activity was started with special instructions from an
-            // Intent, pass the Intent's extras to the fragment as arguments
             firstFragment.setArguments(getIntent().getExtras());
-
-            // Add the fragment to the 'fragment_container' FrameLayout
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.fragment_container, firstFragment).commit();
         }
-      
+
     }
 
-    private void initSpeakBar(){
+    private void initSpeakBar() {
         speakbar = findViewById(R.id.speak);
         play = findViewById(R.id.play);
         del = findViewById(R.id.del);
@@ -167,34 +161,55 @@ public class MainActivity extends AppCompatActivity implements HomeRecyclerAdapt
     }
 
     @Override
-    public void onSubTopicSelected(String title) {
+    public void onSubTopicSelected(final TopicModel model, final View view) {
         Log.e("mylogmessage", "heyb");
-        Toast.makeText(this, title, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, model.getTitle(), Toast.LENGTH_SHORT).show();
 
-        if (res.getIdentifier(title + "_array", "array", getPackageName()) != 0){
-            BasicFragment basicFragment = BasicFragment.newInstance(title);
+        final float originalX = view.getX();
+        final float originalY = view.getY();
+
+        Display display = getWindowManager().getDefaultDisplay();
+        final Point size = new Point();
+        display.getSize(size);
+
+        if (res.getIdentifier(model.getReturnText() + "_array", "array", getPackageName()) != 0) {
+            BasicFragment basicFragment = BasicFragment.newInstance(model.getReturnText());
             transactFragment(basicFragment);
         } else {
-            speakbar.append(title.toUpperCase() + " ");
+            t1.speak(model.getTitle(), TextToSpeech.QUEUE_FLUSH, null, TTS_SPEAK_ID);
+            view.setClickable(false);
+            view.animate().x(size.x / 3f).y(size.y / 3f).translationZBy(10f).scaleXBy(1.25f).scaleYBy(1.25f).setDuration(750).withEndAction(new Runnable() {
+                @Override
+                public void run() {
+                    view.animate().x(originalX).y(originalY).translationZBy(-10f).scaleXBy(-1.25f).scaleYBy(-1.25f).setDuration(1000).withEndAction(new Runnable() {
+                        @Override
+                        public void run() {
+                            view.setClickable(true);
+                        }
+                    });
+                }
+            });
+            speakbar.append(model.getTitle() + " ");
         }
     }
 
-    private void transactFragment(Fragment frag){
+    private void transactFragment(Fragment frag) {
         FragmentTransaction fragmentManager = getSupportFragmentManager().beginTransaction();
         fragmentManager.setCustomAnimations(R.anim.right_in, R.anim.left_out, R.anim.left_in, R.anim.right_out)
                 .replace(R.id.fragment_container, frag, frag.getTag())
                 .addToBackStack(frag.getTag())
                 .commit();
     }
+
     private void updateFragment(Fragment fragment, int bStack) {
         FragmentManager manager = getSupportFragmentManager();
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.replace(R.id.fragment_container, fragment);
-        manager.popBackStackImmediate(1,1);
+        manager.popBackStackImmediate(1, 1);
 
         if (bStack == 1) {
             transaction.addToBackStack(fragment.getTag());
-        } else if (bStack == 0){
+        } else if (bStack == 0) {
             manager.popBackStackImmediate();
         }
         transaction.commit();
@@ -203,5 +218,11 @@ public class MainActivity extends AppCompatActivity implements HomeRecyclerAdapt
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        speakbar.setText("");
     }
 }
