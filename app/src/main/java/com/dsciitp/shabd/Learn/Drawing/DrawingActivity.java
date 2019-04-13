f80c46aeb25155a74cf8a913c501a95f18e7100c
chanmol1999 <chanmol1999@gmail.com>
2019-04-14 00:38:41 +0530

    Implement language setting

--- app/src/main/java/com/dsciitp/shabd/Learn/Drawing/DrawingActivity.java
+++ app/src/main/java/com/dsciitp/shabd/Learn/Drawing/DrawingActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 import com.dsciitp.shabd.R;
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
 
 import java.util.Random;
 
@@ -19,6 +20,10 @@ public class DrawingActivity extends AppCompatActivity {
 
     private DrawableView drawableView;
     private DrawableViewConfig config;
+    private int selectedColorR;
+    private int selectedColorG;
+    private int selectedColorB;
+    private int selectedColorRGB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +38,8 @@ public class DrawingActivity extends AppCompatActivity {
         display. getSize(size);
         int width = size. x;
         int height = size. y-200;
+        final ColorPicker cp = new ColorPicker(DrawingActivity.this, 25, 30, 40);
+
         config= new DrawableViewConfig();
         config.setStrokeColor(getResources().getColor(android.R.color.black));
         config.setShowCanvasBounds(true); // If the view is bigger than canvas, with this the user will see the bounds (Recommended)
@@ -58,8 +65,26 @@ public class DrawingActivity extends AppCompatActivity {
 
             @Override public void onClick(View v) {
                 Random random = new Random();
-                config.setStrokeColor(
-                        Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)));
+                cp.show();
+                Button okColor = cp.findViewById(R.id.okColorButton);
+
+                okColor.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        /* You can get single channel (value 0-255) */
+                        selectedColorR = cp.getRed();
+                        selectedColorG = cp.getGreen();
+                        selectedColorB = cp.getBlue();
+
+                        /* Or the android RGB Color (see the android Color class reference) */
+                        selectedColorRGB = cp.getColor();
+                        config.setStrokeColor(
+                                Color.argb(255, selectedColorR, selectedColorG, selectedColorB));
+                        cp.dismiss();
+                    }
+                });
+
             }
         });
         undoButton.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/dsciitp/shabd/Setting/LanguageSettingActivity.java
@@ -0,0 +1,15 @@
+package com.dsciitp.shabd.Setting;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dsciitp.shabd.R;
+
+public class LanguageSettingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language_setting);
+    }
+}
