8f5bc2bf8295d81aba028deddde01f5bd1a83615
chanmol1999 <chanmol1999@gmail.com>
2019-05-15 20:31:00 +0530

    Implement youtube api to see videos in the app

--- app/src/main/java/com/dsciitp/shabd/Learn/LearnActivity.java
+++ app/src/main/java/com/dsciitp/shabd/Learn/LearnActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -15,6 +14,7 @@ import com.dsciitp.shabd.Learn.ColorGame.ColorGameActivity;
 import com.dsciitp.shabd.Learn.Drawing.DrawingActivity;
 import com.dsciitp.shabd.Learn.Piano.PianoActivity;
 import com.dsciitp.shabd.Learn.Quiz.QuizActivity;
+import com.dsciitp.shabd.Learn.Video.VideoActivity;
 import com.dsciitp.shabd.R;
 
 import java.util.ArrayList;
@@ -23,7 +23,7 @@ public class LearnActivity extends AppCompatActivity implements LearnAdapter.OnC
 
   LearnAdapter storiesAdapter;
 
-    @Override
+    @Overridegit
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
@@ -39,10 +39,11 @@ public class LearnActivity extends AppCompatActivity implements LearnAdapter.OnC
         ArrayList<LearnStoryModel> options = new ArrayList<LearnStoryModel>();
         //options.add( "Draw an apple","dgd","gdfg","https://www.countryplace.com.au/wp-content/uploads/Creative-Painting.jpg","Drawing" );
         options.add( new LearnStoryModel( "Draw an apple","dgd","gdfg","https://i5.walmartimages.ca/images/Large/428/5_r/6000195494285_R.jpg",DrawingActivity.class ) );
-        options.add( new LearnStoryModel( "Play song","dgd","gdfg","https://www.countryplace.com.au/wp-content/uploads/Creative-Painting.jpg",PianoActivity.class ) );
+        options.add( new LearnStoryModel( "Score 5 points","dgd","gdfg","https://www.codester.com/static//uploads/items/2658/preview-xl.jpg",ColorGameActivity.class ) );
         options.add( new LearnStoryModel( "Draw a Bird","dgd","gdfg","https://akm-img-a-in.tosshub.com/indiatoday/images/story/201810/white_stork.jpeg?B2LINO47jclcIb3QCW.Bj9nto934Lox4",DrawingActivity.class ) );
-        options.add( new LearnStoryModel( "Listen to a story","dgd","gdfg","https://s3.amazonaws.com/media.eremedia.com/wp-content/uploads/2018/04/06122011/story.jpeg",DrawingActivity.class ) );
-        options.add( new LearnStoryModel( "Holiday","dgd","gdfg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWEXBiGxKzBLNDhHMmgrR6KBYlOOO2f0SXP5TZ3DS_UthzhOoq",DrawingActivity.class ) );
+        options.add( new LearnStoryModel( "Score 5 points","dgd","gdfg","http://clipartmag.com/images/quiz-clipart-24.jpg",QuizActivity.class ) );
+        options.add( new LearnStoryModel( "Holiday","dgd","gdfg","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWEXBiGxKzBLNDhHMmgrR6KBYlOOO2f0SXP5TZ3DS_UthzhOoq",null ) );
+        options.add( new LearnStoryModel( "Watch 2 videos","dgd","gdfg","https://s3.amazonaws.com/media.eremedia.com/wp-content/uploads/2018/04/06122011/story.jpeg",VideoActivity.class ) );
         storiesAdapter=new LearnAdapter( options,LearnActivity.this, (LearnAdapter.OnCategorySelectedListener )this);
          storyRecycler.setAdapter( storiesAdapter );
 
@@ -92,6 +93,13 @@ public class LearnActivity extends AppCompatActivity implements LearnAdapter.OnC
                 startActivity(new Intent(LearnActivity.this, QuizActivity.class));
             }
         });
+        CardView videoCard = findViewById(R.id.learn_card_3);
+        videoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LearnActivity.this, VideoActivity.class));
+            }
+        });
         CardView colorCard = findViewById(R.id.learn_card_5);
         colorCard.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -118,6 +126,7 @@ public class LearnActivity extends AppCompatActivity implements LearnAdapter.OnC
     @Override
     public void onTopicSelected(LearnStoryModel title) {
 
+        if(title.getIntent()!=null)
         startActivity(new Intent(LearnActivity.this, title.getIntent()));
     }
 }
